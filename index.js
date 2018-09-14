07d734bb15f6720542316544bca4c58d37a85c35
sequeirajonathan <32176675+sequeirajonathan@users.noreply.github.com>
2018-09-14 09:44:53 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var _ = require ('underscore'); // underscore/index.js
+
+// require assumptions
+// Core module
+// File or folder
+// node_modules folder
+
+var testArr = [1,2,3,4];
+console.log(_.contains(testArr,3));
